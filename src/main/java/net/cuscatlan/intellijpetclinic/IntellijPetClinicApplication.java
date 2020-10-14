bf48a9f196f7d9b2f7bfd718525685014eddc163
rbonillab201 <renatio@gmail.com>
2020-10-14 09:20:23 -0600

    Commit inicial

---
+++ src/main/java/net/cuscatlan/intellijpetclinic/IntellijPetClinicApplication.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.intellijpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellijPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/cuscatlan/intellijpetclinic/IntellijPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.intellijpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
